7d597c120aa1c575fa184deb6b6c20f5f7f1625e
Milena <milena.caroline@alunoescolassociais.org.br>
2024-07-04 11:51:05 -0300

    adiciona controller.js

---
+++ controller.js
@@ -0,0 +1,19 @@
+//FUNÇÃO PARA VALIDAÇÃO DE ACESSO  
+ 
+function acessar(){
+    let loginEmail =document.getElementById('loginEmail').value;
+    let loginSenha = document.getElementById('loginSenha').value;
+ 
+    if(!loginEmail || !loginSenha){
+        alert("Favor preencher todos os campos");
+    }else{
+        //alert("Campos preenchidos com sucesso");
+        window.location.href = 'cadastro.html';
+    }
+}
+ 
+// FUNÇÃO PARA ARMAZENAMENTO DE NOMES EM ARRAY
+var dadoLista = [];
+function salvarUser(){
+   
+}
